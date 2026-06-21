d5bb42486acc1173e3835be3dcb5dcb2ebc49e53
wonkotron <wonkotron@gmail.com>
2026-06-21 14:41:41 -0700

    implement reading formation data and returning unit sets

--- fo2szpu.py
+++ fo2szpu.py
@@ -7,11 +7,12 @@ from operator import attrgetter
 # External Dependencies
 import dcs
 
-def to_namespace(data):
+
+def json_data_to_namespace(data):
     """Utility function to cleanly bring data into an object's namespace"""
     if isinstance(data, dict):
         return SimpleNamespace(**{
-            k: to_namespace(v)
+            k: json_data_to_namespace(v)
             for k, v in data.items()
         })
     return data
@@ -28,12 +29,12 @@ def UnitMap(path):
         with open(path, 'r') as json_file:
             data = json.load(json_file)
 
-            return SimpleNamespace(**{k: to_namespace(v) for k, v in data.items()})
+            return SimpleNamespace(**{k: json_data_to_namespace(v) for k, v in data.items()})
     except:
         print("ERROR:  JSON mangled [{0}]".format(path))
         raise
 
-def nation_unit_map_to_pydcs_units(string_data):
+def nation_unit_string_to_pydcs_units(string_data):
     """Parses unit/quantity strings from within the dataset to a list of pydcs objects"""
     # expectation is a comma separated string of the form "pydcs_unit_object, quantity"
 
@@ -45,19 +46,37 @@ def nation_unit_map_to_pydcs_units(string_data):
 
     try:
         unit_ref = attrgetter(unit_str)(dcs)
-        quantity = int(qty_str)
+        qty = int(qty_str)
 
-        return (unit_ref, quantity)
+        return (unit_ref, qty)
     
     except:
         print("ERROR:  mangled data --- unit_str={0} | qty_str={1}".format(unit_str, qty_str))
         raise
 
+def formation_tag_to_pydcs_units(map:SimpleNamespace, faction:str, nation:str, formation_tag:str):   
+    if faction not in ["BLUE", "RED"]:
+        raise ValueError("ERROR:  Invalid faction [{0}]".format(faction))    
+
+    if nation is None:
+        nation = "Default"
+
+    try:
+        unit_type_data = attrgetter("factions.{0}.nation_unit_map.{1}.{2}".format(faction, nation, formation_tag))(map)
+        
+        units = []
+        for formation_data in unit_type_data.unit_list:
+            unit_ref, qty = nation_unit_string_to_pydcs_units(formation_data)
+            
+            units.extend([unit_ref] * qty)
+
+        return units
+
+    except:
+        print("ERROR:  mangled data")
+        raise
 
 if __name__ == "__main__":
     unit_map = UnitMap("unit_map.json")  # TODO: Add input argument
-    
-    unit_ref, qty = nation_unit_map_to_pydcs_units("vehicles.AirDefence.SA_11_Buk_CC_9S470M1, 1")
-    print(unit_ref)
-
-
+    units = formation_tag_to_pydcs_units(unit_map, faction="RED", nation=None, formation_tag="ADA")
+    print(units)
