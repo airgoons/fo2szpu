b9b858db4dd69ed1f7c3d9e50606d7e07ce1ab27
wonkotron <wonkotron@gmail.com>
2026-06-28 15:36:31 -0700

    refactor implementation to utilize new unit_map

--- fo2szpu.py
+++ fo2szpu.py
@@ -7,195 +7,88 @@ from operator import attrgetter
 # External Dependencies
 import dcs
 
-
-def json_data_to_namespace(data):
-    """Utility function to cleanly bring data into an object's namespace"""
-    if isinstance(data, dict):
-        return SimpleNamespace(**{
-            k: json_data_to_namespace(v)
-            for k, v in data.items()
-        })
-    return data
+# Internal Dependencies
+from unit_map import UnitMap, Faction, Nation, Formation
 
 
-class UnitMap:
-    def __init__(self, unit_type_tag_map:dict, factions:dict):
-        self.unit_type_tag_map = unit_type_tag_map
-        self.factions = factions
-
-    @classmethod
-    def from_json(cls, path:str):
-        """Imports unit mapping data from a json"""
-        # Input Validation
-        path = os.path.abspath(path)
-        if not os.path.isfile(path):
-            raise RuntimeError("Path does not exist or is not a file [{0}]".format(self._path))
-
-        data = None
-        try:
-            with open(path, 'r') as json_file:
-                data = json.load(json_file)
-        except:
-            print("ERROR:  JSON mangled [{0}]".format(path))  # TODO:  add logging
-            raise
-
-        return cls(
-                    unit_type_tag_map = data.get("unit_type_tag_map"),
-                    factions = data.get("factions")
-                )
-    
-    class VehicleGroup:
-        def __init__(self, spawn_radius:int, unit_group:list):
-            self.spawn_radius = spawn_radius
-            self.unit_group = unit_group
-
-
-def nation_unit_string_to_pydcs_units(string_data):
-    """Parses unit/quantity strings from within the dataset to a list of pydcs objects"""
-    # expectation is a comma separated string of the form "pydcs_unit_object, quantity"
-
-    try:
-        unit_str, qty_str = [item.strip() for item in string_data.split(",")]  # ignore whitespace
-    except:
-        print("ERROR:  mangled data [{0}]".format(string_data))
-        raise
-
-    try:
-        unit_ref = attrgetter(unit_str)(dcs)
-        qty = int(qty_str)
-
-        return (unit_ref, qty)
-    
-    except:
-        print("ERROR:  mangled data --- unit_str={0} | qty_str={1}".format(unit_str, qty_str))
-        raise
-
-
-def formation_tag_to_pydcs_units(map:SimpleNamespace, faction:str, nation:str, formation_tag:str):   
-    if faction not in ["BLUE", "RED"]:
-        raise ValueError("ERROR:  Invalid faction [{0}]".format(faction))    
-
-    # TODO:  check if nation is mappable for the given faction, if not then Default
-    if nation is None:
-        nation = "Default"
-
-    try:
-        unit_type_data = attrgetter("factions.{0}.nation_unit_map.{1}.{2}".format(faction, nation, formation_tag))(map)
-        
-        units = []
-        for formation_data in unit_type_data.unit_list:
-            unit_ref, qty = nation_unit_string_to_pydcs_units(formation_data)
-            
-            units.extend([unit_ref] * qty)
-
-        return units
-
-    except:
-        print("ERROR:  mangled data")
-        raise
-
-
-class Formation:
-    def __init__(self, name:str, faction:str, nation_tag:str, type_tag:str, position:dcs.mapping.Point, heading:int, vehicle_group:UnitMap.VehicleGroup):
+class MizFormation:
+    def __init__(self, name:str, tags:list, faction:UnitMap.Faction, nation:UnitMap.Nation, formation:UnitMap.Formation):
         self.name = name
+        self.tags = tags
         self.faction = faction
-        self.nation_tag = nation_tag
-        self.type_tag = type_tag
-        self.position = position
-        self.heading = heading
-        self.vehicle_group = vehicle_group
+        self.nation = nation
+        self.formation = formation
+
 
 def formations_from_miz(miz:dcs.Mission, unit_map:UnitMap):
-    drawings = miz.drawings.get_layer_by_name("Author")
-
-    unit_type_tags = unit_map.unit_type_tag_map.values()
+    drawings = miz.drawings.get_layer_by_name(unit_map.miz_drawing_layer)
 
     formations = {}
+    for obj in drawings.objects:
+        if "_label" in obj.name:
+            continue  # reject specific objects
 
-    for faction_name, faction_data in unit_map.factions.items():
-        print("INFO:  Processing faction [{0}]".format(faction_name))  # TODO:  logging
-        nation_tags = faction_data.get("nation_name_tag_map").values()
-        
+        if obj.name in formations:
+            continue  # reject duplicates
 
-        for obj in drawings.objects:
-            if "_label" in obj.name:
-                continue  # reject specific objects
+        name = obj.name
+        tags = name.split("-")[1:]  # NOTE:  assuming no whitespace
+        if len(tags) == 0:
+            print(f"WARN:  tagless object [{name}]")  # TODO:  logging
+            continue  # reject tagless item
 
-            if obj.name not in formations:  # avoid duplication
-                name = obj.name
-                tags = name.split("-")[1:]  # NOTE:  assuming no whitespace
-                faction = None
-                nation_tag = None
-                type_tag  = None
-                
-                if len(tags) == 0:
-                    print("WARN:  tagless object [{0}]".format(obj.name))  # TODO:  logging
-                    continue  # reject tagless items
+        faction = None
+        nation = None
+        formation = None
 
-                if faction_name in tags:
-                    faction = faction_name
+        # Resolve Faction and Nation
+        for tag in tags:
+            # Match faction tag
+            if faction is None:
+                if tag in unit_map.factions.keys():
+                    faction = unit_map.factions.get(tag)
+                    continue  # next tag
 
-                for tag in tags:
-                    if tag in nation_tags:
-                        if nation_tag is not None:
-                            print("WARN:  multiple nation tags [{0}]".format(obj.name))  # TODO:  logging
-                            continue
-                        else:
-                            if faction is None:
-                                faction = faction_name
-
-                            nation_tag = tag
-                        
-                    if tag in unit_type_tags:
-                        if type_tag is not None:
-                            print("WARN:  multiple unit type tags [{0}]".format(obj.name))  # TODO:  logging
-                            continue
-                        else:
-                            type_tag = tag
-
-                if (faction is None) or (nation_tag is None) or (type_tag is None):
-                    print("WARN:  invalid formation [{0}]".format(obj.name))  # TODO:  logging
-                    continue  # invalid formation
+            # Match nation tag
+            if nation is None:
+                if faction is not None:  # easy case first
+                    if tag in faction.nations.keys():
+                        nation = faction.nations.get(tag)
+                        continue  # next tag
                 else:
-                    vehicle_group_raw = unit_map.factions.get(faction).get("nation_unit_map").get(nation_tag).get(type_tag)
-                    unit_group = []
-                    for unit in vehicle_group_raw.get("unit_group"):
-                        unit_ref = attrgetter(unit.get("unit_name"))(dcs)
-                        unit_group.extend([unit_ref] * unit.get("qty"))
-                        
-                    vehicle_group = UnitMap.VehicleGroup(vehicle_group_raw.get("spawn_radius"), unit_group)
-
-                    heading = unit_map.factions.get(faction).get("unit_heading")
-
-                    formations[obj.name] = Formation(name, faction, nation_tag, type_tag, obj.position, heading, vehicle_group)  # NOTE:  duplication checked above
-       
-    return formations
-
-
-def add_groups_from_formations(miz:dcs.Mission, unit_map:UnitMap, formations:dict):
-    for formation_name, formation_obj in formations.items():
-        dcs_country = None
-        if formation_obj.faction == "BLUE":
-            dcs_country = dcs.countries.CombinedJointTaskForcesBlue
-        elif formation_obj.faction == "RED":
-            dcs_country = dcs.countries.CombinedJointTaskForcesRed
+                    for _, f in unit_map.factions.items():
+                        if tag in f.nations.keys():
+                            faction = f
+                            nation = faction.nations.get(tag)
+        
+        if faction is None:
+            print(f"WARN:  invalid formation {name}")
+            continue
         else:
-            raise ValueError("Invalid faction [{0}]".format(formation_obj.faction))
+            if nation is None:
+                nation = faction.get("Default", None)
 
-        group = miz.vehicle_group_platoon(
-            country=dcs_country,
-            name=formation_name,
-            types=formation_obj.vehicle_group.unit_group,
-            position=formation_obj.position,
-            heading=formation_obj.heading
-        )
+        if (faction is not None) and (nation is not None):
+            for tag in tags:
+                if tag in nation.formations.keys():
+                    if formation is not None:
+                        print(f"WARN:  multiple formation tags [{name}]")
+                    else:
+                        formation = nation.formations.get(tag)
+
+        if formation is None:
+            print(f"WARN:  invalid formation {name}")
+        else:
+            formations[name] = MizFormation(name, tags, faction, nation, formation)
+
+    return formations
 
 
 if __name__ == "__main__":
     target_file = "UTNS_ Uprising PRACTICE_fo_export (1).miz"  # TODO:  Add input argument
     unit_map_file = "unit_map.json"    # TODO: Add input argument
     output_file = "{0}_{1}.miz".format(os.path.splitext(target_file)[0], "fo2szpu")  # TODO: Add input argument with default as {target_file}_fo2szpu.miz
-    print("INFO [fo2szpu]:  Configuration:  target_file= {0} | unit_map= {1} | output_file={2}".format(target_file, unit_map_file, output_file))  # TODO:  add logging
+    print(f"INFO [fo2szpu]:  Configuration:  target_file= {target_file} | unit_map= {unit_map_file} | output_file={output_file}")  # TODO:  add logging
 
 
     unit_map = UnitMap.from_json(unit_map_file)
@@ -205,11 +98,10 @@ if __name__ == "__main__":
     print("INFO [fo2szpu]:  Mission Loaded")  # TODO:  add logging
     
     formations = formations_from_miz(miz, unit_map)
-    print("INFO [fo2szpu]:  Formations found= {0}".format(len(formations)))  # TODO:  add logging
+    print(f"INFO [fo2szpu]:  formations detected = {len(formations.keys())}")
 
-    vehicle_groups = add_groups_from_formations(miz, unit_map, formations)
-
-    print("INFO [fo2szpu]:  Saving output: {0}".format(target_file))
+    print(f"INFO [fo2szpu]:  Saving output: {target_file}")
     miz.save(output_file)
+
 
     print("INFO [fo2szpu]:  Done")  # TODO:  add logging
