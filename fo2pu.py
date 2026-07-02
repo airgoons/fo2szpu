758ecd4a7c12e5eefc323990df9628201a7ca9c9
wonkotron <wonkotron@gmail.com>
2026-07-02 16:15:21 -0700

    align messages and output with new project name

--- fo2pu.py
+++ fo2pu.py
@@ -16,15 +16,15 @@ from unit_spawner import UnitSpawner
 if __name__ == "__main__":
     target_file = "UTNS_ Uprising PRACTICE_fo_export (1).miz"  # TODO:  Add input argument
     unit_map_file = "unit_map.json"    # TODO: Add input argument
-    output_file = "{0}_{1}.miz".format(os.path.splitext(target_file)[0], "fo2szpu")  # TODO: Add input argument with default as {target_file}_fo2szpu.miz
-    print(f"INFO [fo2szpu]:  Configuration:  target_file= {target_file} | unit_map= {unit_map_file} | output_file={output_file}")  # TODO:  add logging
+    output_file = "{0}_{1}.miz".format(os.path.splitext(target_file)[0], "fo2pu")  # TODO: Add input argument with default as {target_file}_fo2pu.miz
+    print(f"INFO [fo2pu]:  Configuration:  target_file= {target_file} | unit_map= {unit_map_file} | output_file={output_file}")  # TODO:  add logging
 
 
     unit_map = UnitMap.from_json(unit_map_file)
 
     miz = dcs.Mission(terrain=dcs.terrain.Kola())
     miz.load_file(target_file)
-    print("INFO [fo2szpu]:  Mission Loaded")  # TODO:  add logging
+    print("INFO [fo2pu]:  Mission Loaded")  # TODO:  add logging
     
     formations = Formation.formations_from_miz(miz, unit_map)
 
@@ -32,8 +32,8 @@ if __name__ == "__main__":
 
     groups = UnitSpawner.add_vehicle_sets(miz, vehicle_sets)
 
-    print(f"INFO [fo2szpu]:  Saving output: {output_file}")
+    print(f"INFO [fo2pu]:  Saving output: {output_file}")
     miz.save(output_file)
 
 
-    print("INFO [fo2szpu]:  Done")  # TODO:  add logging
+print("INFO [fo2pu]:  Done")  # TODO:  add logging
